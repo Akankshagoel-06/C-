62d07d9994edaaa5de5c166f5c25ad5e39af7756
Akanksha Goel <67908728+Akankshagoel-06@users.noreply.github.com>
2021-01-20 16:58:46 +0530

    Update duplicate matrix elements to 0.c

--- duplicate matrix elements to 0.c
+++ duplicate matrix elements to 0.c
@@ -1,7 +1,9 @@
+/*Creates a matrix B from an input matrix A but with all duplicate elemnts turned to 0. Prints B and C where C=A-B*/
 int main()
 {
     int r,c,arr[50][50],brr[50][50],crr[50][50],lst[100];
     scanf("%d %d",&r,&c);
+    //Takes input matrix A
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             scanf("%d",&arr[i][j]);
@@ -11,6 +13,7 @@ int main()
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             int f=0;
+            //Checks if element is duplicate or not
             for(int s=0;s<=k;s++){
                 if (lst[s]==arr[i][j]){
                     brr[i][j]=0;
@@ -25,6 +28,7 @@ int main()
             }
         }
     }
+    //prints matrix B
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             printf("%d",brr[i][j]);
@@ -32,6 +36,7 @@ int main()
         printf("\n");
     }
     
+    //Prints matrix C
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             crr[i][j]=arr[i][j]-brr[i][j];
